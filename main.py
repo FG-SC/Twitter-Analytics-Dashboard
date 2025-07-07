00a807335423bb9e30dc2eac4603eee748f78b1c
Felipe Gabriel <felipegabriel.mecanica@gmail.com>
2025-07-07 19:31:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ st.markdown(
     <script async src="https://www.googletagmanager.com/gtag/js?id={GA_MEASUREMENT_ID}"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
+      function gtag(){dataLayer.push(arguments)};
       gtag('js', new Date());
       gtag('config', {GA_MEASUREMENT_ID});
     </script>
